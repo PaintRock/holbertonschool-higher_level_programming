d0ac29d1d398edd0248b688a418f3f516e0f54a3
PaintRock <6126@holbertonstudents.com>
2023-05-11 09:00:14 -0700

    gem

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-worf = str(number)[-1]
+if number < 0:
+        worf = ((number * -1) % 10) * -1
+else 
+worf = number % 10
 if int(worf) > 5:
     print(f"Last digit of {number} is {worf} and is greater than 5")
 elif worf == 0:
     print(f"Last digit of {number} is {worf} and is 0")
-elif int(worf) < 6 and int(worf) != 0:
-    print(f"Last digit of {number} is {worf} and is less than 6 and not 0")
+elif int(worf) < 0:
+    print(f"Last digit of {number} is {-worf} and is less than 6 and not 0")
+else:
+   print(f"Last digit of {number} is {worf} and is less than 6 and not 0") 
