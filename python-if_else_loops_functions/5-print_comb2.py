82440c41a22b8f6f306dcbe9b0419b338650d01f
PaintRock <6126@holbertonstudents.com>
2023-05-17 15:46:16 -0700

    push

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{:02d}".format((i), end="")
-if i != 99:
-print(",", end="")
+for i in range(100):
+       if i == 99:
+                  print("{}".format(i))
+       else:
+                  print("{:02d}, ".format(i), end='')
