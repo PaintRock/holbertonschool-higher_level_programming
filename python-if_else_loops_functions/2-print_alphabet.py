a5aa277b3ad8645e492052ffc4b3bcb0b7052dbe
PaintRock <6126@holbertonstudents.com>
2023-05-17 13:42:50 -0700

    push

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-output = ""
 for i in range(97, 123):
-            output += "{}".format(chr(i))
-            print(output, end='')
-            #!/usr/bin/python3
-#for i in range(97, 123):
- #       print(chr(i), end='')
-        
+    print("{}".format(chr(letter)), end="")    
