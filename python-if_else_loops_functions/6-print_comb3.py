8ff32b4fb16df8fccbd18af59714442d9648d1eb
PaintRock <6126@holbertonstudents.com>
2023-05-18 12:21:37 -0700

    tabs

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for first in range(0, 10):
-        for last in range(0, 10):
-                    if last > first:
-                                    if last > 8 and first > 7:
-                                                        print("{}{}".format(first, last))
-                                    else:
-                                                        print("{}{}".format(first, last), end=", ")
+	for last in range(0, 10):
+		if last > first
+		if last > 8 and first > 7:
+			print("{}{}".format(first, last))
+else:
+	print("{}{}".format(first, last), end=", ")
