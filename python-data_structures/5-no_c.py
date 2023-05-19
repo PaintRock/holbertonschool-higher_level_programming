2bf1d4811de8a32e09dbadd014af5888361a1895
PaintRock <6126@holbertonstudents.com>
2023-05-18 18:25:50 -0700

    no

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,6 @@ def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char == 'c' or char == 'C':
+            continue
             new_string += char
         return new_string
