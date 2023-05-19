28512e17a99d8ac768846df76ed76fed1f466451
PaintRock <6126@holbertonstudents.com>
2023-05-19 15:32:26 -0700

    granola

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Square with size attribute
+"""
+
+
+class Square:
+    """ Defines Square class with private size attribute """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
