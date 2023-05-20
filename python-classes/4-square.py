e5c5414855ecf68726e7b692805fa578dde5c6ac
PaintRock <6126@holbertonstudents.com>
2023-05-20 16:31:25 -0700

    elif not 2nd if

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,18 +7,24 @@ Defines an empty class Square with size attribute
 class Square:
     """ Defines Square class with private size attribute """
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-
-        """property"""
-        def size(self):
-            return self.__size
-
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("value must be an integer")
-        if value < 0:
-            raise ValueError("value must be >= 0")
-        self.__size = value
 
     def area(self):
         return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be and integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
