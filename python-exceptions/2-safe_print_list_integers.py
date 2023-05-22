0d894f5341732663aeb0e62bd0d560c707fccc59
PaintRock <6126@holbertonstudents.com>
2023-05-22 15:40:01 -0700

    petey

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+def safe_print_list_integers(my_list=[], x=0):
+    retro = 0
+    
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                retro += 1
+    except (IndexError, TypeError):
+        pass
+    
+    print()
+    return retro
