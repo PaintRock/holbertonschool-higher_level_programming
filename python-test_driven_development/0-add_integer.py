822274981df59bb006937b159a499adbc4e1acca
PaintRock <6126@holbertonstudents.com>
2023-05-22 16:31:00 -0700

    add_int

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr.bin.python3
+def add_integer(a, b=98)
+"""a function that adds 2 integers"""
+
+"""raises TypeError if either input is not an integer"""
+
+if ((not isinstance(a, int) and not isinstance(a, float))):
+    raise TypeError("a must be an integer")
+if ((not isinstance (b, int) and not isinstance(a, float))):
+    raise TypeError("b must be an integer")
+return (a + b)
