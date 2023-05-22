39b59a438d98a15068f583b087a0f910c0551dcd
PaintRock <6126@holbertonstudents.com>
2023-05-22 16:42:26 -0700

    error codes that mean nothing

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def add_integer(a, b=98)
 """a function that adds 2 integers"""
 
 
-"""raises TypeError if either input is not an integer"""
+def add_integer(a, b=98):
+    """raises TypeError if either input is not an integer"""
 
-if ((not isinstance(a, int) and not isinstance(a, float))):
-    raise TypeError("a must be an integer")
-if ((not isinstance (b, int) and not isinstance(a, float))):
-    raise TypeError("b must be an integer")
-return (a + b)
+
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(a, float))):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
