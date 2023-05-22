5659d2b37caa8549aee79fb1a5ce6ef70aefdb92
PaintRock <6126@holbertonstudents.com>
2023-05-22 16:46:56 -0700

    new

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,6 @@
 def add_integer(a, b=98):
     """raises TypeError if either input is not an integer"""
 
-
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(a, float))):
