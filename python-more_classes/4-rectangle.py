bfea71d1e4a8c7c28bb94371a0faed0ab3f7db8e
PaintRock <6126@holbertonstudents.com>
2023-05-22 19:43:57 -0700

    whitespace

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -62,4 +62,4 @@ class Rectangle:
         return tangleO
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)  
+        return "Rectangle({}, {})".format(self.__width, self.__height)
