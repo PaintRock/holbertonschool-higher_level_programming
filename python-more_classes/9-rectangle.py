85ee6742ed27b86c7a567e30f7bca532e10701f5
PaintRock <6126@holbertonstudents.com>
2023-05-22 21:17:17 -0700

    premie 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -6,11 +6,17 @@
 
 
 class Rectangle:
+    """Class attribute"""
+    number_of_instances = 0
+
+    """Class attribute"""
+    print_symbol = "#"
 
     """include class because it cannot be empty"""
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -46,3 +52,40 @@ class Rectangle:
             return 0
         else:
             return self.width * 2 + self.__height * 2
+
+    """ prints rectangle with # """
+    def __str__(self):
+        tangleO = []
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    tangleO.append(str(self.print_symbol))
+                if i < self.__height - 1:
+                    tangleO.append("\n")
+        tangleO = "".join(tangleO)
+        return tangleO
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    """ returns a Rectangle instance with equal width and height """
+    @classmethod
+    def square(cls, size=0):
+        return cls(width=size, height=size)
