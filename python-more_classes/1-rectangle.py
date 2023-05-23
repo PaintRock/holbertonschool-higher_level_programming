d567bf2dc006c9433fec1c066bb3d31a462328d3
PaintRock <6126@holbertonstudents.com>
2023-05-22 17:26:27 -0700

    documentation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""create an empty class called Rectangle"""
-
-
-"""create the class Rectangle"""
+"""Defines a Rectangle"""
 
 
 class Rectangle:
+    """create the class Rectangle"""
 
     """Instantiation with optional width and height"""
     def __init__(self, width=0, height=0):
