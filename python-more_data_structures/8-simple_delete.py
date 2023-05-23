b5f55617f78b6e5c3b8fe91d0961edaca7d9d7b5
PaintRock <6126@holbertonstudents.com>
2023-05-23 12:39:58 -0700

    whocares

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary is not None:
-        for key in sorted(a_dictionary):
-            print("{}: {}".format(key, a_dictionary[key]))
-    else:
-        print("None")
+    if key in a_dictionary:
+        del a_dictionary[key]
+        return a_dictionary
