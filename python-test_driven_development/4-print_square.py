00b4ef0d96176df84396f0f3320f8b4727d4a22e
PaintRock <6126@holbertonstudents.com>
2023-05-23 16:36:27 -0700

    do not follow instructions

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,16 +7,13 @@
 
 
 def print_square(size):
-"""prints a square, maybe"""
+    """prints a square, maybe"""
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    if not isinstance(size, int) & if size < 0: 
-        raise TypeError("size must be an integer")
 
     for i in range(size):
         for j in range(size):
