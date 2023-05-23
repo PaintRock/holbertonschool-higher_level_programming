7d6b7a40c51fffafcca665507a89994cd8bb8e57
PaintRock <6126@holbertonstudents.com>
2023-05-22 17:08:52 -0700

    try one

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""create an empty class called Rectangle"""
+
+
+"""create the class Rectangle"""
+
+
+class Rectangle:
+
+    """include pass because it cannot be empty"""
+    pass
