9c111b3b628441c80fa5bff9e9847e7ee3aeac0a
PaintRock <6126@holbertonstudents.com>
2023-05-23 13:28:14 -0700

    undo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,1 @@
-#!/usr/bin/python3
-from sys import argv
-    if __name__ == "__main__":    
-       count = len(argv)
-    if count == 1:
-        print("0 argument.")
-    elif count == 2:
-        print("1 argument:")
-    else:
-        print("{:d} arguments:".format(count - 1))
-    for i in range(1, count):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+
