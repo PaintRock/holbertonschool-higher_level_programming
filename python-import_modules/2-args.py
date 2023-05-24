0dda49663b8ba93be9ec414e63ac18b016aca30a
PaintRock <6126@holbertonstudents.com>
2023-05-24 06:40:00 -0700

    why

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
 
+    size = len(argv)
+    if size == 1:
+        print("{} arguments.".format(size - 1))
+    elif size == 2:
+        print("{} argument:".format(size - 1))
+    elif size > 2:
+        print("{} arguments:".format(size - 1))
+    for count in range(1, size):
+        print("{}: {}".format(count, argv[count]))
