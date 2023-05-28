19b3642684c8f00a26b99acb812cd2c37f17d0fc
PaintRock <6126@holbertonstudents.com>
2023-05-28 14:30:55 -0700

    new rulz

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""read a file"""
+
+
 def read_file(filename=""):
+    """Opens and reads a specific file"""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
