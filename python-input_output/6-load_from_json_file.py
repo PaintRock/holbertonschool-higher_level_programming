b905024e5d39370844ba519c2571b21c305eabd4
PaintRock <6126@holbertonstudents.com>
2023-05-28 16:10:04 -0700

    read

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""retrn the JSON reperesentation of an object string"""
+import json
+
+
+def load_from_json_file(filename):
+    """load from json file"""
+    with open(filename, "r", encoding="utf-8") as file:
+        json.loads(file)
