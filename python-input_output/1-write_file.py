0e51a851d3ed186856aeb4d9eddd916917ec3ee0
PaintRock <6126@holbertonstudents.com>
2023-05-28 14:55:06 -0700

    kush

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """Writes to a file"""
-    with open(filename,"w" , encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         charz = file.write(text)
     return charz
