c0ecbeff0b046a929af40c400191581c20da3014
PaintRock <6126@holbertonstudents.com>
2023-05-28 16:21:57 -0700

    return

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """load from json file"""
     with open(filename, 'r', encoding="utf-8") as file:
-        json.loads(file.read())
+       return json.loads(file.read())
