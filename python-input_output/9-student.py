dce826b7ad41d33c721f0bf6d0532fcba1af7bbc
PaintRock <6126@holbertonstudents.com>
2023-05-29 15:56:53 -0700

    me

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module"""
+"""Student class to a dictionary"""
 
 
 class Student:
-    """This method inicializate the atribbutes of the class"""
+    """This method initializes the class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
