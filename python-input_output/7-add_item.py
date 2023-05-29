fd44f7a7f067f21466f82cef1e5ccb221a31df07
PaintRock <6126@holbertonstudents.com>
2023-05-29 15:27:31 -0700

    nextcp

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    arg = sys.argv[1:]
-existing_list = load_from_json_file("add_item.json")
+    existing_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     existing_list = []
 
