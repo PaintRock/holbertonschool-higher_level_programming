fba904fb2dfdce39efa964e3240fecc83aefe6b6
PaintRock <6126@holbertonstudents.com>
2023-05-29 15:16:57 -0700

    us

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,11 +5,17 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    lista = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    lista = []
 
-for element in sys.argv[1:]:
-    lista.append(element)
+    """Extract command-line arguments (excluding the script name)"""
+    arg = sys.argv[1:]
 
-save_to_json_file(lista, "add_item.json")
+    """Load existing list from file if it exists or initialize an empty list"""
+    existing_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        existing_list = []
+
+    """Add command-line arguments to the existing list"""
+    existing_list.extend(args)
+
+    """Save the updated list to the file"""
+    save_to_json_file(existing_list, "add_item.json")
