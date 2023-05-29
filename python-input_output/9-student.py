f27bbdb5449443361f6928e19cc9fc5bad2c8ac4
PaintRock <6126@holbertonstudents.com>
2023-05-29 15:53:29 -0700

    push

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,12 +3,12 @@
 
 
 class Student:
-    
+    """Initializes the attributes of the class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to json(self):
-    """Public method to retrieve a dictionary rep of Student"""
+        """Public method to retrieve a dictionary rep of Student"""
         return self.__dict__
