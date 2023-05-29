185c9e0112ef339b94af64f2edfef819593dbc18
PaintRock <6126@holbertonstudents.com>
2023-05-29 15:54:48 -0700

    ei

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Student Class"""
+"""Module"""
 
 
 class Student:
-    """Initializes the attributes of the class"""
+    """This method inicializate the atribbutes of the class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to json(self):
-        """Public method to retrieve a dictionary rep of Student"""
+    def to_json(self):
+        """This method return a json representation of a class"""
         return self.__dict__
