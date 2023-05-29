1957bf3258703106f8f05da001258d7a80c18bdd
PaintRock <6126@holbertonstudents.com>
2023-05-29 15:33:08 -0700

    will not work right

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,11 +6,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    lista = load_from_json_file("add_item.json")
+    existing_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    lista = []
+    existing_list = []
 
 for element in sys.argv[1:]:
-    lista.append(element)
+    existing_list.append(element)
 
-save_to_json_file(lista, "add_item.json")
+save_to_json_file(existing_list, "add_item.json")
