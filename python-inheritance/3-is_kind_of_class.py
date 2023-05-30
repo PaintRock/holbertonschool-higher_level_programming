c1a10b1af56275accd1d47f232eaec0be2b40b0e
PaintRock <6126@holbertonstudents.com>
2023-05-29 19:40:42 -0700

    lag

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""if the object is an instance [hint: isinstance]"""
+
+def is_kind_of-class(obj, a_class):
+    """True if isinstance of a class that inherited"""
+    return isinstance(obj, a_class)
