1748de875c5e86dab8c83411bb5c26b9500ef10e
PaintRock <6126@holbertonstudents.com>
2023-05-29 17:07:05 -0700

    i do not under stand module documentation

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Write a class Student"""
+"""Module Student"""
 
 
 class Student:
     def __init__(self, first_name, last_name, age):
-        """Init class"""
+        """class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
 
     def to_json(self, attrs=None):
         """public method"""
