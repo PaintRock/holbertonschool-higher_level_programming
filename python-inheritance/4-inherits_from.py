457fb677c69582bddf5436555f31010e3c83a4f3
PaintRock <6126@holbertonstudents.com>
2023-05-30 13:05:20 -0700

    superchecker

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#1/usr/bin/python3
+"""Returns True if the object is an instance that inherited
+directly or indirectly from the specified class"""
+
+
+def inherits_from(obj, a_class)
+    """checks to see if the subclass is a part of the superclass"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
