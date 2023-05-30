ad2eb41f7afdffbbe40e9379eea185cf65c68576
PaintRock <6126@holbertonstudents.com>
2023-05-29 17:33:20 -0700

    git er done

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -19,8 +19,7 @@ class Student:
                 if hasattr(self, item):
                     new_dict[item] = getattr(self, item)
             return new_dict
-    
+
     def reload_from_json(self, json):
         """replaces all attributes of the student init"""
-        for attribute in json: 
-            self.__dict__[attribute] = json[attribute]
+        for attribute in json:
