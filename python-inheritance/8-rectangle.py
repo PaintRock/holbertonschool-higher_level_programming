1c279e15c614b3903e94ec2bdbf29eb8184ae8ab
PaintRock <6126@holbertonstudents.com>
2023-05-30 14:50:12 -0700

    manu

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Class rectangle that inherits from BaseGeometry"""
+"""Method that contain the Base-Geometry class"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Instantiation with private width and height.
-    Validator confirms positive integers"""
+    """Rectangle inherits from BaseGeometry"""
     def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
