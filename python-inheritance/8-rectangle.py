7c744546edace5bf4e90f7ad1bea48450d682974
PaintRock <6126@holbertonstudents.com>
2023-05-30 14:48:13 -0700

    py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class rectangle that inherits from BaseGeometry"""
+"""Class rectangle that inherits from BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
