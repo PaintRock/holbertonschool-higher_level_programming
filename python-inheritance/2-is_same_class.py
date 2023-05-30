c47fca60ac1b90c19bb6d767e82f8e021572d663
PaintRock <6126@holbertonstudents.com>
2023-05-29 19:30:51 -0700

    type

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns true if the object is exactly a match"""
+
+
+def is_same_class(obj, a_class):
+    """It is the same True or False"""
+    return type(obj) == a_class
