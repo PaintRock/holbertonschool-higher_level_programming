41bd18a1a71bbf93339179e6651c64965c509c31
PaintRock <6126@holbertonstudents.com>
2023-05-30 14:46:40 -0700

    py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """class rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-    """Instantiation with private width and height. Validator confirms positive integers"""
+    """Instantiation with private width and height.
+    Validator confirms positive integers"""
     def __init__(self, width, height):
         self.__width = 0
         self.__height = 0
