13d622854d34b58a0c6c19f632b79039a34eeeb7
PaintRock <6126@holbertonstudents.com>
2023-05-31 21:40:12 -0700

    py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,6 +3,25 @@
 
 
 def pascal_triangle(n):
-  """create a pascals triangle"""  
     if n <= 0:
         return []
+
+    result = [[1]]  
+    """ Start with the first row"""
+
+    for i in range(1, n):
+        row_list = [1]  
+        """First element of the row"""
+
+        """ Calculate the elements in between"""
+        for j in range(1, i):
+            """Sum the adjacent elements from the previous row"""
+            element = result[i-1][j-1] + result[i-1][j]
+            row_list.append(element)
+
+        row_list.append(1)  
+        """Last element of the row"""
+        result.append(row_list)  
+        """Add the row to the result list"""
+
+    return result
