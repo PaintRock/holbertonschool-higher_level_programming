c22ea7e78982f6fc23c088eb25429ffdfc76941f
PaintRock <6126@holbertonstudents.com>
2023-06-01 14:58:31 -0700

    why

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,24 @@ class Rectangle(Base):
         self.__height == height
         self.__x = x
         self.__y = y
+
+    def get_width(self):
+        return.self__width
+
+    def set_width(self):
+        self.__width = width
+
+    def get_height(self):
+        return.self__height
+
+    def set_height(self):
+        self.__height = height
+    
+    def get_x(self):
+        return.self__x
+
+    def set_x(self):
+        self.__x = x
+
+    def get_y(self):
+        self.__y = y
