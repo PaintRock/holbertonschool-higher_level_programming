8b6c470893bd6c797075693f1a1127c8739ceb48
PaintRock <6126@holbertonstudents.com>
2023-06-01 14:26:58 -0700

    why

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,9 +2,14 @@
 """Write a class Rectangle that inherits from Base"""
 from models.base import Base
 
+
 class Rectangle(Base):
     """Create a child of Base called Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """def all that is in the rectangle"""
         super().__init__(id)
+        self.__width == width
+        self.__height == height
+        self.__x = x
+        self.__y = y
