acac261880d4edf39ca1b00427884d089920db3d
PaintRock <6126@holbertonstudents.com>
2023-06-01 14:12:46 -0700

    push

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,10 @@
+#1/usr/bin/python3
+"""Write a class Rectangle that inherits from Base"""
+
+
+class Rectangle(Base):
+    """Create a child of Base called Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """def all that is in the rectangle"""
+        super().__init__(id)
