980a341ad29271085ba3f67a11ad763ac29c4f54
PaintRock <6126@holbertonstudents.com>
2023-05-31 21:52:56 -0700

    pywhy

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,22 +6,21 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    result = [[1]]  
     """ Start with the first row"""
+    result = [[1]]
 
     for i in range(1, n):
-        row_list = [1]  
         """First element of the row"""
+        row_list = [1]
 
-        """ Calculate the elements in between"""
-        for j in range(1, i):
-            """Sum the adjacent elements from the previous row"""
-            element = result[i-1][j-1] + result[i-1][j]
-            row_list.append(element)
+    """ Calculate the elements in between"""
+    for j in range(1, i):
+        """Sum the adjacent elements from the previous row"""
+        element = result[i-1][j-1] + result[i-1][j]
+        row_list.append(element)
 
-        row_list.append(1)  
+        row_list.append(1)
         """Last element of the row"""
-        result.append(row_list)  
+        result.append(row_list)
         """Add the row to the result list"""
-
     return result
