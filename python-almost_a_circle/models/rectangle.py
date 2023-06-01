3c1063affbf617ce2677a4e33b10b8e9f59c3cce
PaintRock <6126@holbertonstudents.com>
2023-06-01 14:21:14 -0700

    two

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from models.base import Base
 """Write a class Rectangle that inherits from Base"""
-
+from models.base import Base
 
 class Rectangle(Base):
     """Create a child of Base called Rectangle"""
