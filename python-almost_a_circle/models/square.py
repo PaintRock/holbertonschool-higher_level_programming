3719ff276ee6f76814e3bb053e62323dfea64c27
PaintRock <6126@holbertonstudents.com>
2023-06-02 12:42:35 -0700

    circleone

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,7 +9,13 @@ def __init__(self, size, x=0, y=0, id=None):
     @property
     def size(self):
         return self.width
-    
+
     @size.setter
     def size(self):
         return self.width
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, size, size, x, y)
+
+    dir __str__(self):
+        return f"[Square]({self.id}){self/x}/{self.y} - {self.width}"
