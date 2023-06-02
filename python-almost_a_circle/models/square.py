feb229283230cdceedfdcf0768996130a0213f52
PaintRock <6126@holbertonstudents.com>
2023-06-02 12:47:54 -0700

    checker fixed?

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@ from model.rectangle import Rectangle
 
 
 def __init__(self, size, x=0, y=0, id=None):
+    """Initialize using Rectangle data"""
     super().__init__(size, size, x, y, id)
 
     @property
@@ -14,8 +15,6 @@ def __init__(self, size, x=0, y=0, id=None):
     def size(self):
         return self.width
 
-    def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
-
     dir __str__(self):
+        """We are overloading str or something"""
         return f"[Square]({self.id}){self/x}/{self.y} - {self.width}"
