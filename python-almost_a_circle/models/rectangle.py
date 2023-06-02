54abe4c5e38352704f14e2ac3456387009a961ba
PaintRock <6126@holbertonstudents.com>
2023-06-01 18:38:40 -0700

    dunno

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,7 +17,15 @@ class Rectangle(Base):
     def width(self):
         return self.__width
 
-    @property
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value@property
+    
     def height(self):
         return self.__height
 
@@ -28,15 +36,6 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
 
     @height.setter
     def height(self, value):
