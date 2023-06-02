64bce1e9897afcd4f0afd5e1c8c478547a395fc5
PaintRock <6126@holbertonstudents.com>
2023-06-01 17:53:38 -0700

    go

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,9 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Create a child of Base called Rectangle"""
+    """Create a child class of Base called Rectangle"""
 
     class Rectangle(Base):
+        """Rectangle class initialization"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
@@ -14,26 +15,36 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+    @property
+    """width getter"""
     def get_width(self):
         return self.__width
-
-    def set_width(self, width):
-        self.__width = width
-
+    
+    @property
+    """height getter"""
     def get_height(self):
         return self.__height
 
-    def set_height(self, height):
-        self.__height = height
-
+    @property
     def get_x(self):
         return self.__x
 
-    def set_x(self, x):
-        self.__x = x
-
+    @property
     def get_y(self):
         return self.__y
+    
+    @width.setter
+    def set_width(self, value):
+        self.__width = width
 
-    def set_y(self, y):
+    @x.setter
+    def set_x(self, value):
+        self.__x = x
+
+    @height.setter
+    def set_height(self, value):
+        self.__height = height
+
+    @y.setter
+    def set_y(self, value):
         self.__y = y
