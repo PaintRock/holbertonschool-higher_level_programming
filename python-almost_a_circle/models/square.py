64e38801ebcb8c794d7f216917e6bd51a5e913a5
PaintRock <6126@holbertonstudents.com>
2023-06-02 14:03:29 -0700

    squarely

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,12 +1,56 @@
 #!/usr/bin/python3
-"""Create a class Square from a Rectangle"""
+""" rectangle module contains class Rectangle """
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Initialize the class Square that uses the data from Rectangle"""
+    """ square clase inherits from rectangle """
+
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
+        """ init with arguments and __init__ from Rectangle """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        """ printing of instance square """
+        id_str = str(self.id)
+        w_str = str(self.width)
+        x_str = str(self.x)
+        y_str = str(self.y)
+        div_str = x_str + '/' + y_str + ' - ' + w_str
+        return ("[Square] " + '(' + id_str + ') ' + div_str)
+
+    def update(self, *args, **kwargs):
+        """ 12 update id, width, height, x and y, this order """
+        if args:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.width = args[1] if len(args) >= 2 else self.width
+            self.height = args[1] if len(args) >= 2 else self.height
+            self.x = args[2] if len(args) >= 3 else self.x
+            self.y = args[3] if len(args) >= 4 else self.y
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """ 13 return dict representation of a rectangle """
+        i = self.id
+        w = self.width
