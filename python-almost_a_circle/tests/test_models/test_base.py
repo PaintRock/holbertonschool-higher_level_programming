f3af8329f95bd4e86816e9a8511d766137f84312
PaintRock <6126@holbertonstudents.com>
2023-06-05 09:15:55 -0700

    myine

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,21 +2,15 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    def test_id_assignment(self):
-        '''test the automatic assignment of incremental ids, and the
-        assignment of specific id when provided'''
-        base1 = Base()
-        base2 = Base()
-        base3 = Base(100)
+    def test_base_init_no_id(self):
+        # Test the __init__ method of Base class when no id is provided
+        base = Base()
+        self.assertEqual(base.id, 89)
 
-        self.assertEqual(base1.id, 1)
-        self.assertEqual(base2.id, 2)
-        self.assertEqual(base3.id, 100)
-
-    def test_id_type(self):
-        '''test the type of id attribute to ensure an integer'''
-        base = Base(10)
-        self.assertIsInstance(base.id, int)
+    def test_base_init_with_id(self):
+        # Test the __init__ method of Base class when an id is provided
+        base = Base(42)
+        self.assertEqual(base.id, 42)
 
 if __name__ == '__main__':
     unittest.main()
