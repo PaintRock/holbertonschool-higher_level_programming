a2af963c908ccbb372bfab1cece751f7a7ae15a5
PaintRock <6126@holbertonstudents.com>
2023-06-06 13:22:04 -0700

    width by

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -2,3 +2,14 @@
 import unittest
 from models.rectangle import Rectangle
 from models.base import Base
+
+
+"""Circle tests"""
+
+
+    def test_Square_constructor(self):
+        s = Square(1, 2)
+        self.assertIsNotNone(s)
+        # Additional test for specific dimensions
+        self.assertEqual(s.width, 1)
+        self.assertEqual(s.height, 2)
