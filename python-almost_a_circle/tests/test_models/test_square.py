6885fb43a52d746c54038a92a498a675c7eee7a6
PaintRock <6126@holbertonstudents.com>
2023-06-06 13:40:39 -0700

    squirt

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -8,7 +8,29 @@ from models.base import Base
 
 class TestSqure(unittest.TestCase):
 """Why no one knows"""
+    
+     def setUp(self):
+        """Reset the __nb_objects counter.
+        print test"""
+        print("Square setUp")
+        self.capture_output = io.StringIO()
+        sys.stdout = self. capture_output
 
+        Base._Base__nb_objects = 0
+
+        self.base = Base()
+
+    def tearDown(self):
+        print("Base tearDown")
+
+        sys.stdout = sys.__stdout__
+
+        del self.base
+        try:
+            os.remove("Base.json")
+        except FileNotFoundError:
+            pass
+            
     def test_Square_constructor(self):
         s = Square(1, )
         """does a square exist?"""
