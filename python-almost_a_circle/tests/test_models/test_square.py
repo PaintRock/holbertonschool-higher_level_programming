177aeca2898732f866d038be601a130af00ab07f
PaintRock <6126@holbertonstudents.com>
2023-06-06 13:27:05 -0700

    erase

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -4,13 +4,4 @@ from models.rectangle import Rectangle
 from models.base import Base
 
 
-class TestSqure(unittest.TestCase):
 
-
-    def test_Square_constructor(self):
-        s = Square(1, 2)
-        """does a square exist?"""
-        self.assertIsNotNone(s)
-        # Additional test for specific dimensions
-        self.assertEqual(s.width, 1)
-        self.assertEqual(s.height, 2)
