f95c1c63e0443c940899097e87cd69f2b7a9eb1b
PaintRock <6126@holbertonstudents.com>
2023-06-06 14:44:48 -0700

    delete

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -7,36 +7,3 @@ from models.base import Base
 from models.square import Square
 
 
-class TestSquare(unittest.TestCase):
-    """Why, no one knows"""
-    def setUp(self):
-        """Reset the __nb_objects counter.
-        print test"""
-        print("Bsse setUp")
-        self.capture_output = io.StringIO()
-        sys.stdout = self. capture_output
-
-        Base._Base__nb_objects = 0
-
-        self.base = Base()
-
-    def tearDown(self):
-        print("Base tearDown")
-
-        sys.stdout = sys.__stdout__
-
-        del self.base
-        try:
-            os.remove("Base.json")
-        except FileNotFoundError:
-            pass
-            
-    def test_Square_constructor(self):
-        s = Square(1, )
-        """does a square exist?"""
-        self.assertIsNotNone(s)
-        # Additional test for specific dimensions
-        self.assertEqual(s.width, 1)
-
-if __name__ == '__main__':
-    unittest.main()
