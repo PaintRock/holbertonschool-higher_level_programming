6fa7a2baac95e331cc5a6bcbc7dc577a78c98a97
PaintRock <6126@holbertonstudents.com>
2023-06-24 19:47:24 -0500

    2v1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the database hbtn_0c_0 in your MySQL server.
 
-mysql -h localhost -u root -p -e "CREATE DATABASE IF NOT EXISTS hbtn_0c_0;" 2>/dev/null
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
