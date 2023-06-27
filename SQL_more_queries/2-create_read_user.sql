60e35faec6637fa1396af76ca7cb2eae12c4de3f
PaintRock <6126@holbertonstudents.com>
2023-06-27 13:40:39 -0500

    1-d

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2'@'localhost IDENTIFIED BY user_0d_2_pwd;
 
 -- Grant SELECT privilege to the user on the hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
