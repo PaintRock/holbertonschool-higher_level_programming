0eca9ecf4ecf728e4e0c579b0e55158c635c2281
PaintRock <6126@holbertonstudents.com>
2023-06-27 13:25:29 -0500

    1-b

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- this is a statement
 
-CREATE USER IF NOT EXISTS '{username}'@'localhost' IDENTIFIED BY '{password}';
-GRANT ALL PRIVILEGES ON *.* TO '{username}'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '{password}';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
