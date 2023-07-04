5b8c11a6300a69f56584328edb87ebb24bb1ed2d
PaintRock <6126@holbertonstudents.com>
2023-07-04 15:44:13 -0500

    1v8

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
--- this is the 2nd one
-
-Grants for user_0d_2@localhost
-GRANT USAGE ON *.* TO `user_0d_2`@`localhost`
-GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`
+-- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS  hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
