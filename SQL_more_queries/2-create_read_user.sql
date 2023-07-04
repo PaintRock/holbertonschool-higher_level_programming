0b571d9b57bfa4023195734ce705573efff7d87e
PaintRock <6126@holbertonstudents.com>
2023-07-04 15:39:16 -0500

    1v7 backtick

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- this is the 2nd one
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY `user_0d_2_pwd`;
+GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@`localhost`;
