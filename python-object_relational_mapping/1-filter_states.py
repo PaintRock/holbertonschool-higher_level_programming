19b371a7a32c062625adfd3851cdb52d4cc7ea85
PaintRock <6126@holbertonstudents.com>
2023-07-05 23:38:25 -0500

    1-only Nv3

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,15 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database):
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", 
+                         port=3306, 
+                         user=username, 
+                         passwd=password, 
+                         db=database
+                         )
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
