96ab2b66ed1cba25df908b18e7a80d49ed9d7ead
PaintRock <6126@holbertonstudents.com>
2023-07-05 22:21:46 -0500

    nope

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-""" This module will list all states from the database hbtn_0e_0_usa """
-Import MySQLdb
-Import Sys 
+import MySQLdb
 
-def list_states (
-    username,
-    password, 
-    database
-    ):
-    
-    db = MySQLdb.connect(host="localhost", post=3306, user=username, passwd=password, db=database)
+def list_states(username, password, database):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object to interact with the database
     cursor = db.cursor()
+
+    # Execute the query to retrieve all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query result
     rows = cursor.fetchall()
+
+    # Display the results
     for row in rows:
         print(row)
+
+    # Close the cursor and database connection
     cursor.close()
     db.close()
-    
-    if __name__ == '__main__':
-        import sys
-        
-        if len(sys.argv) != 4:
-            print("Usage: python script.py <username> <password> <database>")
-        else:
-            username = sys.argv[1]
-            password = sys.argv[2]
-            database = sys.argv[3]
-            list_states(username, password, database)
+
+# Provide the MySQL username, password, and database name as command-line arguments
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_states(username, password, database)
