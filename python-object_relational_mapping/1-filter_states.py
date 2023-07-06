30325dae2b09783309832d6feaedb0cf083cfe93
PaintRock <6126@holbertonstudents.com>
2023-07-05 23:27:48 -0500

    me

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,19 +3,16 @@
 import MySQLdb
 import sys
 
-def search_states(username, password, database, state_name):
+def list_states(username, password, database):
     # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
-    # Create the SQL query with user input
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    params = (state_name,)
-
-    # Execute the query with user input
-    cursor.execute(query, params)
+    # Execute the query to retrieve states starting with N (upper N)
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
 
     # Fetch all the rows from the query result
     rows = cursor.fetchall()
@@ -29,14 +26,10 @@ def search_states(username, password, database, state_name):
     db.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
     else:
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
-        state_name = sys.argv[4]
-        search_states(username, 
-                      password, 
-                      database, 
-                      state_name)
+        list_states(username, password, database)
