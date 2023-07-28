2f2265847a466e09d0e51bc318540e2d3517c6db
PaintRock <6126@holbertonstudents.com>
2023-07-28 13:28:47 +0000

    2v1

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Check the number of arguments
+let numOfArgs = process.argv.length - 2;
+
+// Print the message depending on the number of arguments
+if (numOfArgs === 0) {
+  console.log("No argument");
+} else if (numOfArgs === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
