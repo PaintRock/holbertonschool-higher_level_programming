70f6f6de1c610737ec15d0557fc21c16f872c6eb
PaintRock <6126@holbertonstudents.com>
2023-07-28 22:43:38 +0000

    4v3

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-if (args[0] + args[1]) {
-    console.logs args[0] + arg[1]);
-} else {console.log('No argument')
+
+const args = process.argv.slice(2);
+
+if (args[0] && args[1]) {
+  console.log(args[0] + args[1]);
+} else {
+  console.log('No argument');
 }
+
