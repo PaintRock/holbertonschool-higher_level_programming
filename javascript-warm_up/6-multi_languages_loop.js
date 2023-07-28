3891b54c4e0f0e8e49c8ff6d8acf622737799825
PaintRock <6126@holbertonstudents.com>
2023-07-28 23:07:02 +0000

    lint

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* eslint quotes: ["error", "single"], semi: ["error", "always"] */
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
