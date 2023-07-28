9f2e4308cd97f16ae1c2ec676b5326283580d1f0
PaintRock <6126@holbertonstudents.com>
2023-07-28 01:18:27 +0000

    1v1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// First line
+const line1 = "C is fun";
+console.log(line1);
+
+// Second line
+const line2 = "Python is cool";
+console.log(line2);
+
+// Third line
+const line3 = "JavaScript is amazing";
+console.log(line3);
