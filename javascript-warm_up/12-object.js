7103240383f5b37d1eb7259ad2602fb0782bf0df
PaintRock <6126@holbertonstudents.com>
2023-07-31 17:48:46 -0500

    lifers are 2 meaningfu

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 
-function add(a, b) {
-    return a + b;
-  }
-  
-  const firstInteger = parseInt(process.argv[2]);
-  const secondInteger = parseInt(process.argv[3]);
-  
-  if (isNaN(firstInteger) || isNaN(secondInteger)) {
-    console.log("Invalid input. Please provide two integers as arguments.");
-  } else {
-    const result = add(firstInteger, secondInteger);
-    console.log(result);
-  }
-  
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Update the value of 'value' property
+const updatedObject = { ...myObject, value: 89 };
+
+console.log(updatedObject);
