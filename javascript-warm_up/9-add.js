03e1ca211bf108094d3f5fde1ef01ecc94975ffe
PaintRock <6126@holbertonstudents.com>
2023-07-31 21:18:12 +0000

    bry

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
+#!/usr/bin/node
+
 function add(a, b) {
-    return a + b;
-  }
-  
-  const arg1 = process.argv[2];
-  const arg2 = process.argv[3];
-  
-  const num1 = parseInt(arg1, 10);
-  const num2 = parseInt(arg2, 10);
-  
-  if (isNaN(num1) || isNaN(num2)) {
-    console.log("Please provide two valid integers.");
-  } else {
-    const result = add(num1, num2);
-    console.log(result);
-  }
-  
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const num1 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log("Please provide two valid integers.");
+} else {
+  const result = add(num1, num2);
+  console.log(result);
+}
