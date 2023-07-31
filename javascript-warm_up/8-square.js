cb020dd758833fbf95c875ae99882eda0d4bcffd
PaintRock <6126@holbertonstudents.com>
2023-07-31 17:08:01 -0500

    lifers

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,19 +2,19 @@
 const size = process.argv[2];
 
 if (!size || isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   const squareSize = parseInt(size, 10);
-  let square = "";
+  let square = ';
 
   for (let row = 0; row < squareSize; row++) {
     for (let col = 0; col < squareSize; col++) {
-      square += "X";
+      square += 'X';
       if (col < squareSize - 1) {
-        square += " ";
+        square += ' ';
       }
     }
-    square += "\n";
+    square += '\n';
   }
 
   console.log(square);
