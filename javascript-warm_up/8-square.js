a85d91bbe8b274a560fc4895351698d57dd43d54
PaintRock <6126@holbertonstudents.com>
2023-07-31 17:10:47 -0500

    lifers are

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (!size || isNaN(size)) {
   console.log('Missing size');
 } else {
   const squareSize = parseInt(size, 10);
-  let square = ';
+  let square = '';
 
   for (let row = 0; row < squareSize; row++) {
     for (let col = 0; col < squareSize; col++) {
