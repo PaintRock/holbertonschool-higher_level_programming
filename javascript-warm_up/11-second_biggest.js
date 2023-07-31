71b20725a539e79e726aa7919a25614e3cc7ea1e
PaintRock <6126@holbertonstudents.com>
2023-07-31 17:44:21 -0500

    lifers are 2 meaning

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const size = process.argv[2];
+
+if (!size || isNaN(size)) {
+  console.log("Missing size");
+} else {
+  const squareSize = parseInt(size, 10);
+
+  for (let row = 0; row < squareSize; row++) {
+    let squareRow = "";
+    for (let col = 0; col < squareSize; col++) {
+      squareRow += "X";
+    }
+    console.log(squareRow);
+  }
+}
