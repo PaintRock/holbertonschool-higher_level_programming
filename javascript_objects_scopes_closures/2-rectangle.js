c8ea78f797f73c37d515309d1bf3b250d091cc9c
PaintRock <6126@holbertonstudents.com>
2023-08-01 18:26:03 +0000

    2v1

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+    if (w <= 0 || h <= || typeof w !== 'number' || typeof h!== 'number')
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  module.exports = Rectangle;
+  
