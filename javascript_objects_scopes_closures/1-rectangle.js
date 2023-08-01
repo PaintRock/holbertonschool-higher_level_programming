94daa4c30fd086db58c1f7f1a170ef6e33e69027
PaintRock <6126@holbertonstudents.com>
2023-08-01 18:27:46 +0000

    2v1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  if (w <= 0 || h <= || typeof w !== 'number' || typeof h!== 'number')
-  constructor (w, h) {
+    constructor (w, h) {
     this.width = w;
     this.height = h;
   }
--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
 class Rectangle {
-    if (w <= 0 || h <= || typeof w !== 'number' || typeof h!== 'number')
-    constructor (w, h) {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+        // If w or h is equal to 0 or not a positive integer, create an empty object
+        return {};
+      }
+  
       this.width = w;
       this.height = h;
     }
   }
+  
   module.exports = Rectangle;
+  
   
