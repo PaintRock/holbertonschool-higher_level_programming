42d60a500db2cf355474e46be23a65721184850a
PaintRock <6126@holbertonstudents.com>
2023-08-01 18:50:41 +0000

    2v3

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,21 +2,12 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
+    if (w => 0 && h => 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
-
-  toString() {
-    return `Rectangle { width: ${this.width}, height: ${this.height} }`;
-  }
-}
-
-module.exports = Rectangle;
-
+  
+  module.exports = Rectangle;
   
   
