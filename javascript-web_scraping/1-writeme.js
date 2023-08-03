7989b311fb509a190473ed44f1f3035bf6a9d81c
PaintRock <6126@holbertonstudents.com>
2023-08-03 21:04:51 +0000

    1v4

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 /* Write a script that writes the contents of a file */
 const fs = require('fs');
 const path = process.argv[2];
-const text = process.argv[3]
+const text = process.argv[3];
 
 fs.writeFile(path, text, 'utf-8', function (error) {
   if (error) {
