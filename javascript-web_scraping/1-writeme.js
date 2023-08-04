b36c6c1226ab489eee464ae38ba050e531fe713d
PaintRock <6126@holbertonstudents.com>
2023-08-04 03:35:06 +0000

    comments on 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 /* Write a script that writes the contents of a file */
 const fs = require('fs');
-// Import 'fs' (fileStorage) 
+// Import 'fs' (fileStorage)
 const path = process.argv[2];
 // First argument is the filePath
 const string1 = process.argv[3];
 // Second argument is the string to write
+
 fs.writeFile(path, string1, 'utf-8', function (error) {
-  /*instruction to write the file following the
+  /*  Instruction to write the file following the
   path from the first arg, in utf-8, and the function is error */
   if (error) {
     console.log(error);
