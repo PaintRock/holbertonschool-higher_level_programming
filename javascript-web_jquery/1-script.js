3c901d736644e37e6b90e00aa96982c966ea53e0
PaintRock <6126@holbertonstudents.com>
2023-08-06 16:59:51 +0000

    budd

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Holberton School</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <header> 
+        First HTML page
+    </header>
+    <footer>
+        Holberton School - 2017
+    </footer>
+    <script>
+        // Update the text color of the <header> element to red using jQuery
+        $('header').css('color', '#FF0000');
+    </script>
+</body>
+</html>
