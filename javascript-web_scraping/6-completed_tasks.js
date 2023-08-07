f93e56bc90385e1d3ade9a94a095e80a3fcfb7bc
PaintRock <6126@holbertonstudents.com>
2023-08-07 21:21:46 +0000

    6v8

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const data = JSON.parse;
-const length = data.length;
-const countDict = {};
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
+    const data = JSON.parse(body);
+    const length = data.length;
+    const countDict = {};
     for (let i = 1; i <= 10; i++) { // loop through every userId
       let count = 0;
       for (let j = 0; j < length; j++) { // loop through every todo item
