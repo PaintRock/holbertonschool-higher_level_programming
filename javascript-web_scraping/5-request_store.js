c97d043962a0d803287f4f14a2a2cbb51e68c63f
PaintRock <6126@holbertonstudents.com>
2023-08-07 20:49:54 +0000

    5v5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,7 +4,7 @@ const path = process.argv[2];
 const request = require('request');
 const fs = require('fs');
 
-request(url, functon (error, response, body)\ {
+request(url, functon (error, response, body) {
   if(error) {
     console.log(error);
   }else{
