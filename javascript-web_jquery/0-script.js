342240e17d757d867727378845800f19df6cc806
PaintRock <6126@holbertonstudents.com>
2023-08-07 22:48:26 +0000

    2v1

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,18 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Header Text Color Update</title>
-</head>
-<body>
-    <header>This is the header</header>
-
-    <script>
-        // Select the <header> element using document.querySelector
-        const headerElement = document.querySelector('header');
-        
-        // Update the text color to red (#FF0000)
-        headerElement.style.color = '#FF0000';
-    </script>
-</body>
-</html>
-
+// Select the <header> element using document.querySelector
+document.querySelector('HEADER').style.color = '#FF0000';
