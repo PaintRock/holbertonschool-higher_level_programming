9fcdb020a5c795f5d77feedc65bfbf8ecd7e17d5
PaintRock <6126@holbertonstudents.com>
2023-08-07 20:57:17 +0000

    5v7

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,13 +4,3 @@ const path = process.argv[3];
 const request = require('request');
 const fs = require('fs');
 
-request(url, 'utf-8', functon (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {}
-    fs.writeFile(path, 'utf-8', function(error) {
-      if (error) {
-        console.log(error);
-    }
-  })
-});
