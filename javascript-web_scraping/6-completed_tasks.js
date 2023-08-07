8d7b2c35a7724a31ecc07183f07cc6c86b9ecfde
PaintRock <6126@holbertonstudents.com>
2023-08-07 20:29:54 +0000

    6-1

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,39 @@
 #!/usr/bin/node
+const request = require('request');
+
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+
+async function computeCompletedTasks(apiURL) {
+  try {
+    const response = await axios.get(apiURL);
+    const todos = response.data;
+
+    const userCompletedTasks = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        const userId = todo.userId;
+        userCompletedTasks[userId] = (userCompletedTasks[userId] || 0) + 1;
+      }
+    });
+
+    return userCompletedTasks;
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`);
+    return null;
+  }
+}
+
+async function main() {
+  const completedTasks = await computeCompletedTasks(apiURL);
+
+  if (completedTasks) {
+    console.log('Number of completed tasks by user id:');
+    for (const userId in completedTasks) {
+      console.log(`User ID ${userId}: ${completedTasks[userId]} completed tasks`);
+    }
+  }
+}
+
+main();
+
