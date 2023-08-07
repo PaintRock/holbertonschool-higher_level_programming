328666640e444d91a25f491034dd60086dd82eeb
PaintRock <6126@holbertonstudents.com>
2023-08-07 22:58:53 +0000

    3

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+// Update the text color of the <header> element to red using jQuery
+$('header').css('color', '#FF0000');
+// adds a click butoon that turns to red.
+// updates the text color of HTML tag HEADER to red when the user
+// clicks on the tag DIV#red_header
+$('#mad_clicker').click(function () {
+    $('mad_clicker').css('color', '#FF0000');
+  });
