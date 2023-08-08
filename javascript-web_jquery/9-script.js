b3513f2c2dc416f7508eb6eb04e130b59cf6424e
PaintRock <6126@holbertonstudents.com>
2023-08-08 17:38:06 -0500

    9

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,9 +1,4 @@
 // Update the text color of the <header> element to red using jQuery
-// adds a click butoon that turns to red.
-// updates the text color of HTML tag HEADER to red when the user
-// clicks on the tag DIV#red_header
-const url = https://stefanbohacek.com/hellosalut/?lang=fr
-
 $.get("https://stefanbohacek.com/hellosalut/?lang=fr", function(data) {
     $('#hello').text(data.hello);
   });
