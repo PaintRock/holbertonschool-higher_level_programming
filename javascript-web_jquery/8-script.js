d831f643d6ca0ca4784540f743b25db13cc50b11
PaintRock <6126@holbertonstudents.com>
2023-08-08 17:27:30 -0500

    8

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,10 +1,7 @@
 // Update the text color of the <header> element to red using jQuery
-// adds a click butoon that turns to red.
-// updates the text color of HTML tag HEADER to red when the user
-// clicks on the tag DIV#red_header
-$.get('http://swapi.co/api/films?format=json', function (data) {
-  for (film of data['results']) {
-    $('#list_movies').append('<li>' + film['title'] + '</li>');
-  }
-});
-
+$.get("https://swapi-api.hbtn.io/api/films/?format=json", function(data) {
+    data.results.forEach(function(movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
+  
